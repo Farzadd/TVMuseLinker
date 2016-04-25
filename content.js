c56747ced1a38be2e21bda6aa11e4d5703fd35f7
Farzad Daei <farzaddaei@gmail.com>
2016-04-24 23:48:08 -0700

    Version 1.0.0.0 - Initial upload of working code

---
+++ content.js
@@ -0,0 +1,53 @@
+var regExp = /href=&quot;([^&|#]+)\&quot\;/g;
+var validLinks = [];
+
+// http://www.tvmuse.com/tv-shows/*/season_*/episode_*/*
+
+$("#table_search").html("");
+$("[id^=div_com_]").each(onEachComment);
+
+function onEachComment(index, element)
+{
+	if ($(element).find("a")[0]) {
+		sendAction("2h", new Array($(element).attr("id").replace("div_com_", "")));
+	}
+}
+
+// Code borrowed from TVMuse JS files
+function sendAction(action, otherParams)
+{
+	var xmlHttp = new XMLHttpRequest();
+	if (xmlHttp == null)
+  	{
+  		alert ("Your browser does not support AJAX!");
+  		return;
+  	}
+	
+  	var url = '/ajax.php';
+  	var params = "action=" + action + "&sri=" + Math.random();
+
+	if (otherParams)
+		for (i = 0; i < otherParams.length; i++)
+			params = params + '&o_item' + i + '=' + encodeURIComponent(otherParams[i]);
+
+	xmlHttp.open("POST", url, true);
+	xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlHttp.setRequestHeader("Content-length", params.length);
+	xmlHttp.setRequestHeader("Connection", "close");
+	
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			var urls = xmlHttp.responseText.match(regExp);
+			for (i = 0; i < urls.length; i++)
+			{
+				var url = urls[i].replace("href=&quot;", "").replace("&quot;", "");
+				$("#table_search").html($("#table_search").html() + "<li><a href=\"" + url + "\" class=\"list outer cfix\"> <span class=\"c1\"> <em class=\"i_search\">&nbsp;</em> </span> <span class=\"c2 o_hidden\"> <span class=\"block mb_05 nowrap\"><span class=\"bigger bold underline\">" + url + "</span> &nbsp;<span class=\"dark\">by <span class=\"bold\">TVMuse Linker</span></span></span>  </span>  </a>  </li>");
+				validLinks.push(url);
+			}
+		}
+	}
+	
+	xmlHttp.send(params);
+	
+	return false;
+}
